829cef6a37b2c3a478110660a37f2a19eec85fef
Tsolas Georgios <tsolas16@gmail.com>
2022-11-06 10:54:58 +0200

    Added enum Classes

---
+++ src/main/java/com/travelcompany/eshop/enums/CustomerCategory.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.travelcompany.eshop.enums;
+
+/**
+ *
+ * @author Giorgos
+ */
+public enum CustomerCategory {
+    INDIVIDUAL, BUSINESS
+}
