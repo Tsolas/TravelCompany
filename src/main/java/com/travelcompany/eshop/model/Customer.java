e35df3d91a6a9e325fe35666738e1a332bb0135b
Tsolas Georgios <tsolas16@gmail.com>
2022-11-06 11:57:59 +0200

    Added Customer class

---
+++ src/main/java/com/travelcompany/eshop/model/Customer.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.travelcompany.eshop.model;
+
+import com.travelcompany.eshop.enums.CustomerCategory;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Customer {
+
+    private static int id = 0;
+    private String name;
+    private String email;
+    private String address;
+    private String nationality;
+    private CustomerCategory customerCategory;
+
+    public Customer(String name, String email, String address, String nationality, CustomerCategory customerCategory) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.nationality = nationality;
+        this.customerCategory = customerCategory;
+        id++;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{"+"id= " + id + ", name= " + name + ", email= " + email + ", address= " + address + ", nationality= " + nationality + ", customerCategory= " + customerCategory + '}';
+    }
+    
+    
+    
+    
+
+}
