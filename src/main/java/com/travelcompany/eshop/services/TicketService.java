79187b5ed68995f5b15dc3926c970108492608f0
Tsolas Georgios <tsolas16@gmail.com>
2022-11-08 17:11:56 +0200

    Fixed the service layer and deleted unnecessary functionality

--- src/main/java/com/travelcompany/eshop/services/TicketService.java
+++ src/main/java/com/travelcompany/eshop/services/TicketService.java
@@ -2,16 +2,40 @@ package com.travelcompany.eshop.services;
 
 import com.travelcompany.eshop.enums.CustomerCategory;
 import com.travelcompany.eshop.enums.PaymentMethod;
-import com.travelcompany.eshop.model.Customer;
-import com.travelcompany.eshop.model.Itinerary;
-import com.travelcompany.eshop.repository.OrderedTicketRepository;
 
 public interface TicketService {
 
+    /**
+     * This method considers given customer category and payment method to
+     * cumulative discount or surcharge
+     *
+     * @param customerCategory
+     * @param paymentMethod
+     * @return Returns the multiplier which can be used to calculate the final
+     * price
+     */
     double calculateDiscount(CustomerCategory customerCategory, PaymentMethod paymentMethod);
 
+    /**
+     * This method calculates a ticket's final price after discount or extra
+     * charges
+     *
+     * @param price
+     * @param discount
+     * @return Returns final price of the ticket
+     */
     double calculatePrice(double price, double discount);
-
-    void orderTicket(Customer customer, Itinerary itinerary, PaymentMethod paymentMethod, OrderedTicketRepository ticketRepository);
-
 }
+
+//    /**
+//     * This method saves a finalized ordered ticket after calculating discount
+//     * and final price
+//     *
+//     * @param customer
+//     * @param itinerary
+//     * @param paymentMethod
+//     * @param ticketRepository
+//     * @see calculateDiscount
+//     * @see calculatePrice
+//     */
+//    void orderTicket(Customer customer, Itinerary itinerary, PaymentMethod paymentMethod, OrderedTicketRepository ticketRepository);
--- src/main/java/com/travelcompany/eshop/services/TicketServiceImplementation.java
+++ src/main/java/com/travelcompany/eshop/services/TicketServiceImplementation.java
@@ -2,22 +2,9 @@ package com.travelcompany.eshop.services;
 
 import com.travelcompany.eshop.enums.CustomerCategory;
 import com.travelcompany.eshop.enums.PaymentMethod;
-import com.travelcompany.eshop.model.Customer;
-import com.travelcompany.eshop.model.Itinerary;
-import com.travelcompany.eshop.model.OrderedTicket;
-import com.travelcompany.eshop.repository.OrderedTicketRepository;
 
 public class TicketServiceImplementation implements TicketService {
 
-    /**
-     * This method considers given customer category and payment method to
-     * cumulative discount or surcharge
-     *
-     * @param customerCategory
-     * @param paymentMethod
-     * @return Returns the multiplier which can be used to calculate the final
-     * price
-     */
     @Override
     public double calculateDiscount(CustomerCategory customerCategory, PaymentMethod paymentMethod) {
         double priceMultipier;
@@ -35,42 +22,8 @@ public class TicketServiceImplementation implements TicketService {
 
     }
 
-    /**
-     * This method saves a finalized ordered ticket after calculating discount
-     * and final price
-     *
-     * @param customer
-     * @param itinerary
-     * @param paymentMethod
-     * @param ticketRepository
-     * @see calculateDiscount
-     * @see calculatePrice
-     */
-    @Override
-    public void orderTicket(Customer customer, Itinerary itinerary, PaymentMethod paymentMethod, OrderedTicketRepository ticketRepository) {
-        int customerId = customer.getId();
-        int itineraryId = itinerary.getId();
-        OrderedTicket ticket = new OrderedTicket(customerId, itineraryId, paymentMethod);
-        CustomerCategory category = customer.getCustomerCategory();
-        double discount = this.calculateDiscount(category, paymentMethod);
-        double price = itinerary.getPrice();
-        double paymentAmount = this.calculatePrice(price, discount);
-        ticket.setPaymentAmount(paymentAmount);
-        ticketRepository.insertOrderedTicket(ticket);
-
-    }
-
-    /**
-     * This method calculates a ticket's final price after discount or extra
-     * charges
-     *
-     * @param price
-     * @param discount
-     * @return Returns final price of the ticket
-     */
     @Override
     public double calculatePrice(double price, double discount) {
         return price + (price * discount);
     }
-
 }
