c046ee54e9ff08ca60f2ceb7b14bdc642643f12f
Georgios Tsolas <tsolas16@gmail.com>
2022-11-09 20:12:28 +0200

    Added enums for airport codes

---
+++ src/main/java/com/travelcompany/eshop/enums/DepartureAirportCodes.java
@@ -0,0 +1,5 @@
+package com.travelcompany.eshop.enums;
+
+public enum DepartureAirportCodes {
+    ATH, BER, LIS, MAD, BAR;
+}
---
+++ src/main/java/com/travelcompany/eshop/enums/DestinationAirportCodes.java
@@ -0,0 +1,5 @@
+package com.travelcompany.eshop.enums;
+
+public enum DestinationAirportCodes {
+    PAR, LON, AMS, DUB, FRA, MEX, SKG, BTU, YXU, NOP;
+}
