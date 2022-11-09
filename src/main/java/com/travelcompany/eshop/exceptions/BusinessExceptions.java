4d9e00e6c5eb2f75bca5cfb1d8cda39b011c19b9
Georgios Tsolas <tsolas16@gmail.com>
2022-11-09 19:06:56 +0200

    Added Exception Classes

---
+++ src/main/java/com/travelcompany/eshop/exceptions/BusinessExceptions.java
@@ -0,0 +1,8 @@
+package com.travelcompany.eshop.exceptions;
+
+public class BusinessExceptions extends Exception {
+
+    public BusinessExceptions(String message) {
+        super(message);
+    }
+}
